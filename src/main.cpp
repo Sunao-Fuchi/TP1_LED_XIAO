41b65a50e0dd0ea1231d18709e32489a336bc5fc
Florian <florian.pernet.mugnier.kerr@gmail.com>
2026-09-22 10:22:28 +0200

    Variable de délai ajouté, délai reglé à 200ms

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h> 
+
+int delaiMs = 200; //délai en milliseondes
+
 void setup() { // GPIO21 = LED_BUILTIN : LED embarquée de la XIAO ESP32-S3 
     pinMode(LED_BUILTIN, OUTPUT);   // configure la broche en sortie 
 } 
 
 void loop() { 
     digitalWrite(LED_BUILTIN, LOW);   // LED allumée (logique inversée) 
-    delay(1000);                      // attente 1000 ms = 1 s 
+    delay(delaiMs);                      // attente 1000 ms = 1 s 
     digitalWrite(LED_BUILTIN, HIGH);  // LED éteinte 
-    delay(1000);                      // attente 1 s 
+    delay(delaiMs);                      // attente 1 s 
 }
